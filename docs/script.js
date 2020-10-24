85a2134952afd4916ec448bd8d09b9fd1063721e
Shibungi <narukamino708@gmail.com>
2020-10-24 18:58:05 +0900

    add how to use

--- docs/script.js
+++ docs/script.js
@@ -4,6 +4,8 @@ let Y;
 let N = 4;
 
 let Board = []
+let initialBoard = []
+
 let BoardSize = 500;
 let BoardX = 50;
 let BoardY = 50;
@@ -15,31 +17,45 @@ let RotateX = 0;
 let RotateY = 0;
 
 let Move = 0;
+let BestMove;
 
+let Cleared = 0;
+let ClearedMove = 0;
+
+let CorrectBoard = [[1,2,3,4],[5,6,7,8],[9,10,11,12],[13,14,15,16]];
 
 function setup(){
 	X = 800;
 	Y = 600;
+
+	for(let i = 0;i < N;i++){
+		initialBoard[i] = [];
+		for(let j = 0;j < N;j++){
+			initialBoard[i][j] = 0;
+		}
+	}
+	make_new_problem();
 	for(let i = 0;i < N;i++){
 		Board[i] = [];
 		for(let j = 0;j < N;j++){
-			Board[i][j] = i*N + j + 1;
+			Board[i][j] = initialBoard[i][j];
 		}
 	}
 	createCanvas(X,Y);
 	//背景色
 	background(0);
-	textAlign(CENTER, CENTER);
-	textSize(24);
 }
 
 function draw(){
 	background(0);
 	drawBoard();
 	drawRotateArea();
+	drawData();
 }
 
 function drawBoard(){
+	textAlign(CENTER, CENTER);
+	textSize(24);
 	noStroke();
 	let PanelSize = BoardSize/N;
 	let p = 0.9;
@@ -67,11 +83,15 @@ function drawRotateArea(){
 function keyPressed(){
 	if(key === 'q'){
 		Move = 0;
-		for(let i = 0;i < N;i++){
-			for(let j = 0;j < N;j++){
-				Board[i][j] = i*N + j + 1;
-			}
-		}
+		Cleared = 0;
+		BoardCopy(Board,initialBoard);
+	}
+
+	if(key === 'p'){
+		Move = 0;
+		Cleared = 0;
+		make_new_problem();
+		BoardCopy(Board, initialBoard);
 	}
 
 	if(key === ' '){
@@ -127,3 +147,110 @@ function Rotate(RotateDir){
 	}
 }
 
+function make_new_problem(){
+	let moves = getRandomInt(4,8);
+	let Boards = [];
+	for(let i = 0;i <= moves;i++){
+		Boards[i] = [];
+		for(let j = 0;j < N;j++){
+			Boards[i][j] = [];
+			for(let k = 0;k < N;k++){
+				Boards[i][j][k] = j*N + k + 1;
+			}
+		}
+	}
+	for(let i = 0;i < moves;i++){
+		let next_state;
+		let flag = 0;
+		while(flag === 0){
+			next_state = get_random_next_state(Boards[i]);
+			flag = 1;
+			for(let j = 0;j <= i;j++){
+				if(next_state.toString() === Boards[j].toString()){
+					flag = 0;
+					break;
+				}
+			}
+		}
+		Boards[i+1] = next_state;
+	}
+	// initialBoard = Boards[moves];
+	BoardCopy(initialBoard, Boards[moves]);
+	BestMove = moves;
+}
+
+function get_random_next_state(state){
+	let next_state = [];
+	for(let i = 0;i < N;i++){
+		next_state[i] = [];
+		for(let j = 0;j < N;j++){
+			next_state[i][j] = state[i][j];
+		}
+	}
+	let RotateSize = getRandomInt(RotateSizeMin,RotateSizeMax+1);
+	let x = getRandomInt(0,N-RotateSize+1);
+	let y = getRandomInt(0,N-RotateSize+1);
+	let dir = getRandomInt(0,2);
+	let Panel = [];
+	for(let i = 0;i < RotateSize;i++){
+		Panel[i] = [];
+		for(let j = 0;j < RotateSize;j++){
+			Panel[i][j] = state[i+y][j+x];
+		}
+	}
+	if(dir === 0){
+		for(let i = 0;i < RotateSize;i++){
+			for(let j = 0;j < RotateSize;j++){
+				next_state[i+y][j+x] = Panel[j][RotateSize-1-i];
+			}
+		}
+	}else if(dir === 1){
+		for(let i = 0;i < RotateSize;i++){
+			for(let j = 0;j < RotateSize;j++){
+				next_state[i+y][j+x] = Panel[RotateSize-1-j][i];
+			}
+		}
+	}
+	return next_state;
+}
+
+function getRandomInt(min, max) {
+	min = Math.ceil(min);
+	max = Math.floor(max);
+	return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
+}
+
+function BoardCopy(a,b){
+	for(let i = 0;i < N;i++){
+		for(let j = 0;j < N;j++){
+			a[i][j] = b[i][j];
+		}
+	}
+}
+
+function drawData(){
+	fill(255);
+	textAlign(LEFT,TOP);
+	textSize(20);
+	let textX = BoardX*2 + BoardSize;
+	let textY = BoardY;
+	text("Best Move : " + String(BestMove),textX,textY);
+	text("Your Move : " + String(Move),textX,textY*2);
+
+	if(Cleared === 0 && Board.toString() === CorrectBoard.toString()){
+		Cleared = 1;
+		ClearedMove = Move;
+	}
+	if(Cleared === 1){
+		text("CLEAR!!",textX,BoardY*5);
+		text("Your Move was " + String(ClearedMove),textX,textY*6);
+	}
+
+	text("How to use",BoardX*2 + BoardSize,textY*8);
+	textSize(16);
+	text("c : Counterclockwise",BoardX*2 + BoardSize,textY*9);
+	text("v : Clockwise",BoardX*2 + BoardSize,textY*9.5);
+	text("space : Resize",BoardX*2 + BoardSize,textY*10);
+	text("q : Reset",BoardX*2 + BoardSize,textY*10.5);
+	text("p : New Problem",BoardX*2 + BoardSize,textY*11);
+}
